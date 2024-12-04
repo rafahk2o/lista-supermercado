a7e8353506e1ee42990215534bcdf71f44679910
rafahk2o <49276128+rafahk2o@users.noreply.github.com>
2024-12-04 09:17:44 -0300

    docker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'standalone',
+  experimental: {
+    serverActions: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
